9b6446bfddf5b3df1a4465d19fa3251466b15f5d
Neizzzy <azat.azatch@yandex.ru>
2025-09-24 18:11:43 +0300

    Add UrlWithLastCheckDTO

---
+++ app/DTO/UrlWithLastCheckDTO.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Hexlet\Code\DTO;
+
+readonly class UrlWithLastCheckDTO
+{
+    public function __construct(
+        public int $id,
+        public string $name,
+        public string $createdAt,
+        public ?string $checkCreatedAt,
+        public ?int $statusCode
+    ) {
+    }
+}
